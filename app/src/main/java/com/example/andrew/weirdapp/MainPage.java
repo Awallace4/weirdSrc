aec5fdcd8513c6a3737ede30dd5d124d01db696c
Andrew <awallace4@crimson.ua.edu>
2016-06-12 11:48:36 -0500

    added toolbar and images

--- app/src/main/java/com/example/andrew/weirdapp/MainPage.java
+++ app/src/main/java/com/example/andrew/weirdapp/MainPage.java
@@ -1,5 +1,7 @@
 package com.example.andrew.weirdapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainPage extends AppCompatActivity {
 
@@ -15,6 +18,18 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main_page);
+
+        ImageButton websiteButton = (ImageButton)findViewById(R.id.websitePortal);
+        websiteButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                Uri uri = Uri.parse("https://mfweird.com/");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -32,9 +47,7 @@ public class MainPage extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
